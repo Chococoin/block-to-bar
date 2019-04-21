211ba34f82d216bda7c4a18ef0e0dcc6d3b9ad79
Germán Lugo <german.lugo@bitnibs.com>
2019-04-21 16:50:06 +0200

    Setup to heroku.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ async function krakenApiCall(){
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
