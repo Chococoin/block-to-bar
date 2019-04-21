4c1db921e95c7b81dedcb0375e934c6dc93e91ba
Germán Lugo <german.lugo@bitnibs.com>
2019-04-21 08:49:18 +0200

    Add port for production.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ async function krakenApiCall(){
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.listen(PORT, function() {
