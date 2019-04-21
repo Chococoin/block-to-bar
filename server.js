1fdbd6291019126b9e1fcfd0bda2dd94a39947fb
Germán Lugo <german.lugo@bitnibs.com>
2019-04-21 16:44:17 +0200

    Setup to heroku.

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-      res.sendFile(path.join('build', 'index.html'));
+      res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
     });
 }
 
